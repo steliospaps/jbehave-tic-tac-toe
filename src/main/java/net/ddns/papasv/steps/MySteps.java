71f54685221adcfcae27ed66eca4ee8a623d191a
stelios <stelios05@gmail.com>
2015-04-03 14:04:52 +0100

    initial from archetype

---
+++ src/main/java/net/ddns/papasv/steps/MySteps.java
@@ -0,0 +1,10 @@
+package net.ddns.papasv.steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class MySteps {
+    
+}
