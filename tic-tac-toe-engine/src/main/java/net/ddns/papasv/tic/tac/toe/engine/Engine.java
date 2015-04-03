bc4621ad6f251524dae9fdd90d3788a3051e63a3
stelios <stelios05@gmail.com>
2015-04-03 22:38:27 +0100

    not finished games identified

--- tic-tac-toe-engine/src/main/java/net/ddns/papasv/tic/tac/toe/engine/Engine.java
+++ tic-tac-toe-engine/src/main/java/net/ddns/papasv/tic/tac/toe/engine/Engine.java
@@ -1,9 +1,47 @@
 package net.ddns.papasv.tic.tac.toe.engine;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.ObjIntConsumer;
+import java.util.function.Supplier;
+
 public class Engine {
 
-	public void evaluate(String game) {
-		throw new RuntimeException("bad input "+game);
+	/**
+	 * 
+	 * @param game a string with numbers 1-9 representing the movements. first move is first player, second is second etc.
+	 * @return a result see {@link Result}
+	 */
+	public Result evaluate(String game) {
+		game.codePoints()
+			.map(a->toNumber(a))
+			.sequential()
+			.collect( () -> new Set[]{new HashSet<Integer>(),new HashSet<Integer>()},
+					(r,i)->accumulate(r,i), 
+					(a,b)->doThrow());
+		return Result.NOT_FINISHED;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void accumulate(Set[] set, int i) {
+		Set<Integer> a = set[0];
+		Set<Integer> b = set[1];
+		if(a.contains(i) || b.contains(i)){
+			throw new RuntimeException("duplicate value "+i);
+		}
+		Set<Integer> active = a.size()>b.size()?b:a;
+		active.add(i);
+	}
+
+	private Object doThrow() {
+		throw new RuntimeException("I did not expect this to be called");
+	}
+
+	private int toNumber(int codePoint) {
+		if(Character.isDigit(codePoint)){
+			return Character.digit(codePoint, 10);
+		}
+		throw new RuntimeException("character is not a number "+codePoint+" "+Character.toTitleCase(codePoint));
 	}
 
 }
--- tic-tac-toe-engine/src/test/java/net/ddns/papasv/tic/tac/toe/engine/EngineTest.java
+++ tic-tac-toe-engine/src/test/java/net/ddns/papasv/tic/tac/toe/engine/EngineTest.java
@@ -1,5 +1,6 @@
 package net.ddns.papasv.tic.tac.toe.engine;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -18,5 +19,19 @@ public class EngineTest {
 	public void badInputShouldThrow() {
 		engine.evaluate("foo");
 	}
+	@Test(expected=RuntimeException.class)
+	public void badInputShouldThrow1() {
+		engine.evaluate("11");
+	}
+	@Test(expected=RuntimeException.class)
+	public void badInputShouldThrow2() {
+		engine.evaluate("121");
+	}
+	
+	@Test public void incomplete_games() {
+		assertThat(engine.evaluate("1"), equalTo(Result.NOT_FINISHED));
+		assertThat(engine.evaluate("53289146"), equalTo(Result.NOT_FINISHED));
+	}
+	
 
 }
